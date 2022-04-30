72f4d5cbdf7d00fe072b0ca19fce334d8ea6c9ff
Anthony Leonel Chaisa Fernandez <achaisa@unsa.edu.pe>
2022-04-29 19:21:04 -0500

    Se agrego el metodo div que retorna un entero

--- Calculadora.java
+++ Calculadora.java
@@ -2,6 +2,9 @@ public class Calculadora{
 	//Funcion add(+)
 	//Funcion sub(-)
 	//Funcion div(/)
+		public static int div(int a, int b){
+			return a / b;
+		}
 	//Funcion mul(*)
 	//Funcion mod(%)
 }
