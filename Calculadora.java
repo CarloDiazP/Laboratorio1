6246a3c728eec5af0e65a235b45489c0fd285511
Anthony Leonel Chaisa Fernandez <achaisa@unsa.edu.pe>
2022-04-29 19:23:17 -0500

    Se corrigio la identación

--- Calculadora.java
+++ Calculadora.java
@@ -2,9 +2,9 @@ public class Calculadora{
 	//Funcion add(+)
 	//Funcion sub(-)
 	//Funcion div(/)
-		public static int div(int a, int b){
-			return a / b;
-		}
+	public static int div(int a, int b){
+		return a / b;
+	}
 	//Funcion mul(*)
 	//Funcion mod(%)
 }
